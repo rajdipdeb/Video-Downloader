95a92f90f7d2d98e6dfe47b62d02803f8d5929de
rajdipdeb <dipdeb880212@gmail.com>
2025-05-07 02:08:25 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 function downloadVideo() {
-  const url = document.getElementById("videoURL").value;
-  if (url) {
-    const youtubeID = url.split("v=")[1];  // Extract YouTube video ID
-    window.open("https://ssyoutube.com/watch?v=" + encodeURIComponent(youtubeID));  // Redirect to download page
+  const url = document.getElementById("urlInput").value;
+  const message = document.getElementById("message");
+
+  if (!url) {
+    message.innerText = "Please paste a video URL.";
+    return;
+  }
+
+  if (url.includes("youtube.com") || url.includes("youtu.be")) {
+    const newUrl = "https://ssyoutube.com/watch?v=" + url.split("v=")[1];
+    window.open(newUrl, "_blank");
+    message.innerText = "Opening download page...";
+  } else if (url.includes("facebook.com")) {
+    window.open("https://fdown.net", "_blank");
+    message.innerText = "Redirecting to Facebook downloader...";
+  } else if (url.includes("instagram.com")) {
+    window.open("https://igram.io", "_blank");
+    message.innerText = "Redirecting to Instagram downloader...";
   } else {
-    alert("Please enter a video URL.");
+    message.innerText = "Unsupported URL. Please try YouTube, Facebook, or Instagram.";
   }
 }
